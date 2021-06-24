c26ac6823932b18dff9a7b67fe4c0dbe0173910e
Cloud User <centos@ip-172-31-14-90.us-east-2.compute.internal>
2021-06-24 20:58:13 +0000

    Added database stuff, and register page

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,14 @@
 from whitenoise import WhiteNoise
-
-from flask import Flask
-from flask import render_template
-
+from flask import Flask, request, render_template
 from app.portfolio import portfolio
+import os
+from . import db
 
 app = Flask(__name__)
-
+app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
+db.init_app(app)
 app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
-
 app.register_blueprint(portfolio)
-
 
 @app.errorhandler(404)
 def not_found(error_message):
--- app/portfolio.py
+++ app/portfolio.py
@@ -1,3 +1,5 @@
+from werkzeug.security import generate_password_hash
+from app.db import get_db
 from flask import Blueprint, render_template
 from dotenv import load_dotenv
 
@@ -50,3 +52,33 @@ def resume():
         title=title,
         description=description,
     )
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = f"User {username} is already registered."
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return f"User {username} created successfully"
+        else:
+            return error, 418
+
+    ## TODO: Return an actual register page
+    return "Register Page not yet implemented", 501
