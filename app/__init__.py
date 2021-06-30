0b1b240ae273cc1a33875e240366a9b09cc9ed57
Cloud User <centos@ip-172-31-14-90.us-east-2.compute.internal>
2021-06-30 20:32:22 +0000

    Dockerized Database

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,84 @@
 from whitenoise import WhiteNoise
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from app.portfolio import portfolio
 import os
-from . import db
 
 app = Flask(__name__)
-app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
-db.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}'.format(
+            user=os.getenv('POSTGRES_USER'),
+            passwd=os.getenv('POSTGRES_PASSWORD'),
+            host=os.getenv('POSTGRES_HOST'),
+            port=5432,
+            table=os.getenv('POSTGRES_DB'))
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
 app.register_blueprint(portfolio)
 
 @app.errorhandler(404)
 def not_found(error_message):
     return render_template("404.html"), 404
+
+
+class UserModel(db.Model):
+    __tablename__ = 'users'
+
+    username = db.Column(db.String(), primary_key=True)
+    password = db.Column(db.String())
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        error = None
+   
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif UserModel.query.filter_by(username=username).first() is not None:
+            error = f"User {username} is already registered."
+   
+        if error is None:
+            new_user = UserModel(username, generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
+            return render_template("login.html")
+        else:
+            return error, 418
+
+    ## TODO: Return an actual register page
+    return render_template("register.html")
+   
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        error = None
+        user = UserModel.query.filter_by(username=username).first()
+        
+        if user is None:
+             error = 'Incorrect username.'
+        elif not check_password_hash(user.password, password):
+             error = 'Incorrect password.'
+             
+        if error is None:
+             return render_template("index.html")
+        else:
+            return error, 418
+    
+    return render_template("login.html")
--- app/portfolio.py
+++ app/portfolio.py
@@ -1,7 +1,5 @@
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask import Blueprint, render_template, request
 from dotenv import load_dotenv
-from app.db import get_db
 
 load_dotenv()
 
@@ -52,55 +50,3 @@ def resume():
         title=title,
         description=description,
     )
-
-@portfolio.route('/register', methods=('GET', 'POST'))
-def register():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        db = get_db()
-        error = None
-
-        if not username:
-            error = 'Username is required.'
-        elif not password:
-            error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
-            error = f"User {username} is already registered."
-
-        if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
-            return render_template("login.html")
-        else:
-            return error, 418
-
-    ## TODO: Return an actual register page
-    return render_template("register.html")
-
-@portfolio.route('/login', methods=('GET', 'POST'))
-def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        db = get_db()
-        error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
-
-        if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password.'
-
-        if error is None:
-            return render_template("index.html")
-        else:
-            return error, 418
-    return render_template("login.html")
