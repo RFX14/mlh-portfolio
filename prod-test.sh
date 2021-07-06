2ab0d30b5385156ca310c19d072207c3904f9782
coffeemates <jrosa1756@gmail.com>
2021-07-06 14:56:33 -0500

    Fixed error code confusion

--- prod-test.sh
+++ prod-test.sh
@@ -6,9 +6,9 @@ resp=$(curl -s -w "%{http_code}" GET $url)
 result=${resp: -3}
 if [[ $result == 200 ]]; then
     echo "Test 1: PASS"
-    exit 1
+    exit 0
 else 
     echo "Test 1: FAIL, Result: $result"
-    exit 0
+    exit 1
 fi
 
